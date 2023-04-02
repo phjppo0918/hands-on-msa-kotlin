60126097459b005f25688fee71459bb50f3732bd
Hyeonjun Park <phjppo0918@gmail.com>
2023-04-02 16:36:00 +0900

    feat : init setting

---
+++ src/main/kotlin/com/example/handsonmsakotlin/HandsOnMsaKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.handsonmsakotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HandsOnMsaKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<HandsOnMsaKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/handsonmsakotlin/HandsOnMsaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.handsonmsakotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HandsOnMsaKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
